c0ea46e329ba47a95e8b5c1a461a4409def63fd2
Dmitry Korotovsky <rmpic30@gmail.com>
2013-07-14 18:14:03 +0400

    initial

---
+++ src/korotovsky/composerYiiModule/components/SimpleComponent.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Dmitry Korotovsky <dmitry.korotovsky@gmail.com>
+ * @copyright 2013
+ * @license The BSD 3-Clause License http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace korotovsky\composerYiiModule;
+
+
+class SimpleComponent extends \CComponent {
+    public function init()
+    {
+        echo 'hello!';
+    }
+}
