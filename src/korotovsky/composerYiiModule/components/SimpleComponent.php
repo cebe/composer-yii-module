0e2a5ea3e35384763a8bccfcd2a39f32f1921684
Dmitry Korotovsky <rmpic30@gmail.com>
2013-07-14 18:20:14 +0400

    Update SimpleComponent.php

--- src/korotovsky/composerYiiModule/components/SimpleComponent.php
+++ src/korotovsky/composerYiiModule/components/SimpleComponent.php
@@ -5,7 +5,7 @@
  * @license The BSD 3-Clause License http://opensource.org/licenses/BSD-3-Clause
  */
 
-namespace korotovsky\composerYiiModule;
+namespace korotovsky\composerYiiModule\components;
 
 
 class SimpleComponent extends \CComponent {
